bdce9dcb140bc6bd57afdb232e5514567019e795
BexHeider <heider.zapa@bexsoluciones.com>
2024-12-25 12:06:53 -0500

    adding to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Filament\Facades\Filament;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Filament::serving(function () {
-            Filament::registerRenderHook('auth.pages.login.after', fn () => redirect('/'));
-        });
     }
 }
